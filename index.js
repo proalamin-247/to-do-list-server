8d8c28bdff3cb0c0ea6af83f73f9c9f6e4c79fcb
proalamin-247 <pro.alamin247@gmail.com>
2022-05-18 20:44:03 +0600

    server set up and connect mongoDb

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t2b9n.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const taskList = client.db('toDo').collection('task')
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res)=>{
+    res.send('Running to-do-list node server')
+});
+
+app.listen(port, () => {
+    console.log('curd server is running port - 5000')
+})
